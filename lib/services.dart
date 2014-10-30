7a2d01c4d5cac31a2c98f3201f1ef767153d2d21
Pierre Reliquet <pierre.reliquet@gmail.com>
2014-10-30 21:48:27 +0100

    Adding the step3 defining the first service

---
+++ lib/services.dart
@@ -0,0 +1,6 @@
+library angulardart_flight_school_services;
+ 
+import 'package:angular/angular.dart';
+import 'package:angulardart_flight_school/model/contact.dart';
+ 
+part 'services/contacts.dart';
--- web/addressbook.dart
+++ web/addressbook.dart
@@ -1,6 +1,15 @@
 library addressbook;
 import 'package:angular/application_factory.dart';
+import 'package:angular/angular.dart';
+import 'package:angulardart_flight_school/services.dart';
 
 main() {
-  applicationFactory()..run();
+  main() {
+    applicationFactory()
+        ..addModule(
+          new Module() // Creating an inline module based on method cascade
+            ..bind(Contacts) // Registering our Contacts service to have it available through DI
+          )
+        ..run();
+  }
 }
